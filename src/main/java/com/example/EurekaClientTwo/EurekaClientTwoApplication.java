406b1f99a1d9afbd4f285c807e7a450bcb984624
talkLinux2me <167791277+talkLinux2me@users.noreply.github.com>
2024-09-06 11:22:08 -0400

    first commit

---
+++ src/main/java/com/example/EurekaClientTwo/EurekaClientTwoApplication.java
@@ -0,0 +1,15 @@
+package com.example.EurekaClientTwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class EurekaClientTwoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaClientTwoApplication.class, args);
+	}
+
+}
